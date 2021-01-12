f8e09934aee3985ecfa1009d54dbd340042d9ee8
Dennis Zierahn <dezi@kappa-mm.de>
2021-01-12 14:15:26 +0100

    Check in.

--- linux/hci/smp.go
+++ linux/hci/smp.go
@@ -9,7 +9,7 @@ import (
 const (
 	pairingRequest           = 0x01 // Pairing Request LE-U, ACL-U
 	pairingResponse          = 0x02 // Pairing Response LE-U, ACL-U
-	pairingConfirm           = 0x03 // Pairing Confirm LE-U
+	pairingConfirm           = 0x03 // Pairing ConfirmYes LE-U
 	pairingRandom            = 0x04 // Pairing Random LE-U
 	pairingFailed            = 0x05 // Pairing Failed LE-U, ACL-U
 	encryptionInformation    = 0x06 // Encryption Information LE-U
